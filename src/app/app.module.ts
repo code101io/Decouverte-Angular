40d3aade71bd938ff141883169d0ba7e7f6303ad
Code 101 <code101@MacBook-Pro-de-Kevin.local>
2020-09-01 17:12:39 +0200

    ajout de component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomepageViewComponent } from './homepage-view/homepage-view.component';
+import { ListComponent } from './homepage-view/list/list.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageViewComponent,
+    ListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/homepage-view/homepage-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage-view',
+  templateUrl: './homepage-view.component.html',
+  styleUrls: ['./homepage-view.component.scss']
+})
+export class HomepageViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
