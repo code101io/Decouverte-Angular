f3fd47d23de8a09252135734c0e32128d6c1b4af
Code 101 <code101@MacBook-Pro-de-Kevin.local>
2020-10-09 17:09:43 +0200

    ajout des tests e2e

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display first h1 title', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('DBMovies app is running!');
+    expect(page.getHeadingText()).toEqual("Movie's list");
   });
 
   afterEach(async () => {
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -5,7 +5,7 @@ export class AppPage {
     return browser.get(browser.baseUrl) as Promise<unknown>;
   }
 
-  getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+  getHeadingText(): Promise<string> {
+    return element(by.css('app-root h1')).getText() as Promise<string>;
   }
 }
