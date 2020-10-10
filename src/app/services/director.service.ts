bcc0f7cdac7a6799a96cea67d393630f37605674
Code 101 <code101@MacBook-Pro-de-Kevin.local>
2020-10-10 15:33:11 +0200

    Ajout des environments

--- src/app/services/director.service.ts
+++ src/app/services/director.service.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../environments/environment';
 import { Observable } from 'rxjs';
 import { Director } from './../interfaces/director';
 import { HttpClient } from '@angular/common/http';
@@ -8,27 +9,29 @@ import { Injectable } from '@angular/core';
 })
 export class DirectorService {
 
+  apiUrl = environment.apiUrl;
+
   constructor(
     private readonly httpClient: HttpClient
   ) { }
 
   getDirectors(): Observable<Director[]> {
-    return this.httpClient.get<Director[]>('/api/directors');
+    return this.httpClient.get<Director[]>(`${this.apiUrl}/directors`);
   }
 
   getDirector(id: string): Observable<Director> {
-    return this.httpClient.get<Director>(`/api/directors/${id}`);
+    return this.httpClient.get<Director>(`${this.apiUrl}/directors/${id}`);
   }
 
   createDirector(director: Director): Observable<Director> {
-    return this.httpClient.post<Director>('/api/directors', director);
+    return this.httpClient.post<Director>(`${this.apiUrl}/directors`, director);
   }
 
   updateDirector(director: Director): Observable<Director> {
-    return this.httpClient.put<Director>(`/api/directors/${director.id}`, director);
+    return this.httpClient.put<Director>(`${this.apiUrl}/directors/${director.id}`, director);
   }
 
   deleteDirector(id: string): Observable<void> {
-    return this.httpClient.delete<void>(`/api/directors/${id}`);
+    return this.httpClient.delete<void>(`${this.apiUrl}/directors/${id}`);
   }
 }
